d5615f7af64a5bcc3350a533a872002a60669533
Riaz Ur Rahaman <rahaman.riaz@gmail.com>
2026-09-16 23:15:14 +0400

    feat(g1): Chess960 (Fischer Random) start-position generation + castling rules

--- server.js
+++ server.js
@@ -332,6 +332,7 @@ const ALLOWED_FILES = new Set([
   'a11y-text-entry.js',
   'a11y-gestures.js',
   'voice-intents.js',
+  'chess960.js',
   'manifest.webmanifest',
   'service-worker.js',
   'CBURNETT-LICENSE.txt'
